be199938ca6de19d12cc385ee763ffbc33dbb230
giovana moura <gimoura.oli@gmail.com>
2026-08-20 00:21:49 -0300

    solução exercicío 4

---
+++ .history/ex4_20260820001629.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    int ano;
+
+    printf("Digite o ano: ");
+    scanf("%d", &ano);
+
+    if
---
+++ ex4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int ano;
+
+    printf("Digite o ano: ");
+    scanf("%d", &ano);
+
+    if (ano % 4 == 0 && ano % 100 != 0 || ano % 400 == 0) {
+        printf("O ano e bissexto.\n");
+    } else {
+        printf("O ano nao e bissexto.\n");
+    }
+    return 0;
+}
